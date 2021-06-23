5b2610203d30b447b9a7ef413badd9801e877cf9
Akash Kumar <rajputAkashCoding@gmail.com>
2021-06-24 00:27:06 +0530

    netflix app complete with ReactJs using components, props, and Array map function

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        imgsrc: "https://wallpapercave.com/uwp/uwp374924.jpeg",
+        title: "A Netflix Original Series",
+        sname: "Dark",
+        link: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp7418473.jpg",
+        title:"A Netflix Original Series",
+        sname:"Extra Curriculur",
+        link :"https://www.netflix.com/in/title/80990668"
+    },
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp7150696.jpg",
+        title:"A Netflix Original Series",
+        sname:"Lucifer",
+        link :"https://www.netflix.com/in/title/80057918"
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp4961946.jpg",
+        title: "A Netflix Original Series",
+        sname: "Money Heist",
+        link: "https://www.netflix.com/in/title/80192098"
+    },
+    {
+        imgsrc: "https://wallpapercave.com/wp/wp8864719.jpg",
+        title: "A Netflix Original Series",
+        sname: "Dark Desire",
+        link: "https://www.netflix.com/in/title/81090319"
+    }
+]
+
+export default Sdata
